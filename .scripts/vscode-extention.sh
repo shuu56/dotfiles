b09a86ffe8e9179ab70eedfed04580b258535a80
Shuichi Tokuda <shuichitokuda@ShuichinoMacBook-Pro.local>
2020-03-15 21:20:53 +0900

    test

--- .scripts/vscode-extention.sh
+++ .scripts/vscode-extention.sh
@@ -2,6 +2,6 @@
 
 code --list-extensions > ~/dotfiles/.vscode/extentions
 
-sh ~/dotfiles/.scripts/git-add-commit-push.sh "auto commit for vscode extentions"
+sh ~/dotfiles/.scripts/git-add-commit-push.sh "auto commit for vscode extentions" >/dev/null 2>$1
 
 exit 0
